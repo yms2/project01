5384ddd725d02e2b9f09b1fd8f7f391ff9d133dc
DESKTOP-R7TQ6PG\dbals <dbalstjd789@daum.net>
2022-11-19 15:15:05 +0900

    컨텐츠 추가

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
 const menu = document.querySelector(".menuButton")
 const layer = document.querySelector(".gnbAllLayer")
 const xbutton = document.querySelector(".gnbLayerBt")
+const nav = document.querySelector(".slide-menu")
 
 menu.addEventListener('click',()=>{
     layer.style.display = "block"
-    layer.style.transform = "translate3d(-100%,0,0);"
+    layer.style.display = "block"
 })
 
 xbutton.addEventListener('click',()=>{
     layer.style.display = "none"
 })
 
-const slideList =document.querySelector(".noticeBn")
-const slideCont = document.querySelectorAll(".swiper-slide")
-const contLen = slideCont.length;
 
-slideList.style.height = `${130 * (contLen + 1)}px`
+const navBar = document.querySelectorAll(".nav-cate1")
+// const nav = document.querySelector(".slide-menu")
+
+navBar.addEventListener('click',() =>{
+    nav.style.display = "block"
+})
